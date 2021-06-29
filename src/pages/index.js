4c8dcac84fc43b741a562a4bbda4cd5af6cf779b
llyuutoll <daily.harada.y@gmail.com>
2021-06-29 10:26:17 +0900

    feat: Add SEO and top page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
-import React from "react"
+import React from "react";
+import { graphql, /*Link*/ } from 'gatsby';
+import Container from '../components/container';
+import SEO from '../components/seo';
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = ({ data }) => {
+    const siteTitle = data.site.siteMetadata.title;
+    const siteDescription = data.site.siteMetadata.description;
+    return (
+        <Container>
+            <SEO title={siteTitle} description={siteDescription} />
+
+        </Container>
+    );
+};
+
+export default Home;
+
+export const pageQuery = graphql`
+{
+    site {
+        siteMetadata {
+            title
+            description
+        }
+    }
 }
+`;
