6f48a80b3b5e3dd781c1d20bfd12e1c990c5945f
llyuutoll <daily.harada.y@gmail.com>
2021-06-29 10:19:01 +0900

    feat: Implement header and footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,42 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+    siteMetadata: {
+        title: "llyuutoll's Space.",
+        author: {
+            name: "Yuuto",
+            summary: "The university student",
+        },
+        description: "llyuutollのポートフォリオです.",
+        siteUrl: "https://llyuutoll.github.io",        
+        social: {
+            twitter: `@tanqcircle`
+        },
+    },
+    plugins: [
+        
+        `gatsby-plugin-sharp`,
+        `gatsby-remark-images`,
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: [".mdx", ".md"],
+                gatsbyRemarkPlugin: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1200,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/blog`,
+                name: `blog`,
+            },
+        },
+    ],
+};
